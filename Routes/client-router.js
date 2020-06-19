f9396210d3bbfb640b00e0868c03f1f8dd8e5715
unknown <m_fattih@hotmail.com>
2020-06-18 19:51:02 -0700

    added to getAllClass Feature

--- Routes/client-router.js
+++ Routes/client-router.js
@@ -15,4 +15,33 @@ router.get("/", checkJWT, async (req, res) => {
   }
 });
 
+router.get("/", checkJWT, async (req, res) => {
+  try {
+    const data = await dbClient.findAllClient();
+    if (data.length > 0) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ err: "Couldn't find any client" });
+    }
+  } catch (err) {
+    res.status(500).json({ err: "Server Error" });
+  }
+});
+
+/// didnt require JWT for  front-end usage will add later
+
+// router.get("/class", checkJWT, async (req, res) => {
+router.get("/class", async (req, res) => {
+  try {
+    const data = await dbClient.findAllClientClass();
+    if (data.length > 0) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ err: "Couldn't find any class" });
+    }
+  } catch (err) {
+    res.status(500).json({ err: "Server Error" });
+  }
+});
+
 module.exports = router;
